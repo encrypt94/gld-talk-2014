2cedec834417937074760e6df59cce59a4e3390c
encrypt <encrypt94@gmail.com>
2014-09-04 21:34:53 +0200

    add tamago example

---
+++ examples/tamago/tamago.rb
@@ -0,0 +1,15 @@
+require 'arduino_firmata'
+
+@arduino = ArduinoFirmata.connect(nil, nonblock_io: true, eventmachine: false)
+@arduino.pin_mode(13, ArduinoFirmata::OUTPUT)
+
+def tap(pin)
+  @arduino.digital_write(pin, ArduinoFirmata::HIGH)
+  sleep 0.025
+  @arduino.digital_write(pin, ArduinoFirmata::LOW)
+  sleep 0.025
+end
+
+99999.times do
+  tap 13
+end
