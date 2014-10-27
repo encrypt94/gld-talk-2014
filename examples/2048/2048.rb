f43c0609057ca5f31a74444a32c2da57171baf49
encrypt <encrypt94@gmail.com>
2014-10-27 17:58:53 +0100

    refactor example and add arduino stuff

--- examples/2048/2048.rb
+++ examples/2048/2048.rb
@@ -1,8 +1,35 @@
 require 'adb-sdklib'
 require 'c2048'
+require 'arduino_firmata'
 
 adb = AdbSdkLib::Adb.new
 device = adb.devices.first
+
+@arduino = ArduinoFirmata.connect(nil, nonblock_io: true, eventmachine: false)
+
+#Configure pins as output and set them to low
+
+4.times do |pin|
+  @arduino.pin_mode(pin+2, ArduinoFirmata::OUTPUT)
+  @arduino.digital_write(pin+2, ArduinoFirmata::HIGH)
+end
+
+def swipe(dir)
+  pins = {
+    left: [3, 2],
+    right: [2, 3],
+    up: [5, 4],
+    down: [4, 5]
+  }
+  @arduino.digital_write(pins[dir][0], ArduinoFirmata::LOW)
+  sleep 0.50
+  @arduino.digital_write(pins[dir][1], ArduinoFirmata::LOW)
+  sleep 0.10
+  @arduino.digital_write(pins[dir][0], ArduinoFirmata::HIGH)
+  sleep 0.25
+  @arduino.digital_write(pins[dir][1], ArduinoFirmata::HIGH)
+end
+
 
 points = [
          {x: 120, y: 260},
@@ -26,30 +53,14 @@ points = [
 
 def color_to_value(color)
   numbers = {
-    "-4280160" => {
-      value: 0
-    },
-    "-1121062" => {
-      value: 2
-    },
-    "-1187640" => {
-      value: 4
-    },
-    "-872071" => {
-      value: 8
-    },
-    "-682653" => {
-      value: 16
-    },
-    "-623521" => {
-      value: 32
-    },
-    "-631237" => {
-      value: 64
-    },
-    "-1192078" => {
-      value: 128
-    }
+    "-4280160" =>   0,
+    "-1121062" =>   2,
+    "-1187640" =>   4,
+    "-872071"  =>   8,
+    "-682653"  =>  16,
+    "-623521"  =>  32,
+    "-631237"  =>  64,
+    "-1192078" => 128
   }
   unless numbers.has_key?(color.to_s)
     numbers.each do |key, value|
@@ -64,38 +75,32 @@ def color_to_value(color)
   return nil
 end
 
-last_value = 0
-
-screen = device.screenshot
-
-lols = [-20,0,20]
-a = 0
-1.times do 
-  values = []
+loop do 
   screen = device.screenshot
+  row_counter = 0
+  values = []
   points.each do |point|
-    a += 1
-    cl = 0
-    v = nil
-    3.times do |i|
-      cl = screen.pixel(point[:x]+lols[i],point[:y]+i)
-      puts cl
-      vl = color_to_value(cl)
-      if(vl)
-        v = vl
+    row_counter += 1
+    value = nil
+    [-20,0,20].times do |d|
+      color = screen.pixel(point[:x]+d,point[:y]).argb
+      v = color_to_value(color)
+      if(v)
+        value = v
       end
     end
-    if(v)
-      print v[:value].to_s+" "
-      values.push(v[:value])
+    if(value)
+      print value.to_s+" "
+      values.push(v)
     else
-      print cl.to_s+" "
+      print "?"
     end
-    if a == 4
-      a = 0
+    if row_counter == 4
+      row_counter = 0
       print "\n"
     end
   end
-  puts C2048::AI.best_move(values)
-  sleep 3
+  move = C2048::AI.best_move(values)
+  puts m
+  swipe(move)
 end
